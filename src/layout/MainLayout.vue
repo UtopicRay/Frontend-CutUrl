ef05243ddc7e960afb9cfc91914b87cdc39f48b0
UtopicRay <rayrm@gmail.com>
2024-09-29 00:22:49 -0400

    'Create MainLayout.vue'

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Contact from "@/components/Contact.vue";
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <v-layout>
+    <v-app-bar color="primary" prominent>
+      <Header/>
+    </v-app-bar>
+    <v-main>
+      <slot></slot>
+    </v-main>
+    <!--<v-footer  style="background-color:#FFEEAD">
+      <Contact/>
+    </v-footer>-->
+  </v-layout>
+  
+</template>
+
+<style scoped>
+
+</style>
