9cccf21d89889ad1d3cd086bf7c987ab1f75d944
UtopicRay <rayrm@gmail.com>
2024-10-23 01:59:40 -0400

    'add function generateQR'

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,19 +1,45 @@
-<script setup>
-import URL from "@/assets/img/url-2.png"
+<script lang="ts" setup>
+import URL from "@/assets/img/url-2.png";
+import {useService} from "@/hook/useService";
+import {toRef} from "vue";
+import Loader from "@/components/Loader.vue";
+const url=toRef<String>('');
+const {image,error,loading,generateQR,isActive}=useService()
+
+if (error){
+  console.log(error)
+}
 </script>
 
 <template>
-<v-row>
-  <v-col class="d-flex justify-center  align-center">
-    <v-form class="d-flex flex-column justify-start">
-      <v-text-field placeholder="Introduce your URL" required ></v-text-field>
-      <v-btn @click="">Get your url</v-btn>
-    </v-form>
-  </v-col>
-  <v-col>
-    <v-img :src="URL" height="400" width="400"></v-img>
-  </v-col>
-</v-row>
+  <v-container id="qr">
+    <v-toolbar color="transparent" density="comfortable">
+      <v-toolbar-title class="text-center font-weight-black text-h4" > Get and share your QR</v-toolbar-title>
+    </v-toolbar>
+    <v-row>
+      <v-col class="d-flex justify-center text-center text-md-left flex-column">
+        <p class="my-4">
+          Boost your brand’s visibility and engagement with
+          <br>our customizable QR code generator.
+          <br>Quick, easy, and effective!
+        </p>
+        <v-form class="d-flex flex-column justify-start align-center align-md-start mx-auto w-100">
+          <v-text-field class="w-75" placeholder="Introduce your URL" required v-model="url"></v-text-field>
+          <v-btn @click="generateQR(url)" max-width="200">Get your url</v-btn>
+        </v-form>
+      </v-col>
+      <v-col>
+        <v-img :src="URL" height="400" width="400" v-if="!isActive"/>
+        <div class="w-100 mx-auto d-flex justify-center align-center" v-if="loading">
+          <Loader/>
+        </div>
+        <v-img :src="image"  width="300" height="300" class="w-100" v-if="image" />
+      </v-col>
+    </v-row>
+    <v-toolbar v-if="image&&!loading" color="transparent">
+      <v-toolbar-title v-if="!image.success" class="text-red text-center" >{{image.message}}</v-toolbar-title>
+    </v-toolbar>
+  </v-container>
 </template>
 
 <style scoped>
--- src/hook/useService.ts
+++ src/hook/useService.ts
@@ -1,11 +1,15 @@
-import {toRef} from "vue";
+import {ref, toRef} from "vue";
 import {Url_api} from "@/types";
+import {urlStore} from "@/store";
 
 export function useService() {
 
     const results=toRef<Url_api>('');
     const loading=toRef<Boolean>(false);
     const error=toRef<String>('');
+    const isActive=toRef<boolean>(false);
+    const image=ref();
+    const store=urlStore();
 
     async function shortUrl(url:string) {
        try {
@@ -17,13 +21,37 @@ export function useService() {
                },
                body:JSON.stringify({original_url:url})
            }).then(response=>response.json())
-               .then(data=>results.value=data);
+               .then(data=> {
+                   results.value = data
+                   store.addUrl(results);
+               });
        }catch(e){
            error.value=e.message;
        }finally {
-           setInterval(()=>loading.value = false,3000);
+           setInterval(()=>loading.value = false,2000);
        }
    }
 
-    return {results,loading,error,shortUrl}
+    async function generateQR(url:string) {
+        try {
+            isActive.value = true;
+            loading.value = true;
+            await fetch('http://localhost:8000/genQr',{
+                method:'POST',
+                headers:{
+                    "Content-Type":"application/json",
+                },
+                body:JSON.stringify({original_url:url})
+            }).then(response=>response.blob())
+                .then(data=> {
+                    console.log(data)
+                    image.value = URL.createObjectURL(data);
+                });
+        }catch(e){
+            error.value=e.message;
+        }finally {
+            setInterval(()=>loading.value = false,2000);
+        }
+    }
+    return {results,loading,error,shortUrl,generateQR,image,isActive};
 }
