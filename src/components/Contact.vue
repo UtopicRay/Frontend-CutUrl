932a6474cc965a699d4b445bc504db172ec18eba
UtopicRay <rayrm@gmail.com>
2024-10-25 02:47:19 -0400

    'Contact Full Complete'

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,18 +1,14 @@
 <template>
-  <v-footer border>
+  <v-footer border class="bg-amber-accent-1 text-center flex justify-center flex-column" id="contact">
     <div>
-      <h2 class="text-gray-300 ">Ray Rendon</h2>
-      <div class="flex gap-2">
+      <h2 class="text-gray-300 ">Let connect!!</h2>
+      <div class="flex gap-4 justify-center ">
         <a href="https://www.linkedin.com/in/ray-rendon-mesa-1948b7260">
-          <v-icon icon="mdi mdi-linkedin" class="text-white text-4xl hover:scale-125 transition-all duration-200 size-10"></v-icon>
+          <v-icon icon="mdi mdi-linkedin" class="text-black text-4xl hover:scale-125 transition-all duration-200 size-10"></v-icon>
         </a>
         <a href="https://github.com/UtopicRay">
-          <v-icon icon="mdi mdi-github" class="text-white text-4xl hover:scale-125 transition-all duration-200 size-10"></v-icon>
+          <v-icon icon="mdi mdi-github" class="text-black  text-4xl hover:scale-125 transition-all duration-200 size-10"></v-icon>
         </a>
-      </div>
-    </div>
-    <div>
-      <div class="flex items-center">
         <a
             class="flex justify-center items-center text-white  hover:scale-110 transition-all duration-200  underline"
             href="mailto:rayrm2021@gmail.com?subject=Interesado%20en%20sus%20servicios"
@@ -24,6 +20,8 @@
         </a>
       </div>
     </div>
+    <div>
+    </div>
   </v-footer>
 </template>
 
@@ -32,7 +30,6 @@
 </script>
 
 <style scoped>
-
 .social-link :deep(.v-icon){
   text-decoration: none;
   transition: .2s ease-in-out;
@@ -41,5 +38,4 @@
   &:hover{
     color: rgba(25, 118, 210, 1);
   }
-
 </style>
