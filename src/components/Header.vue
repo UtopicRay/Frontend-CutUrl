83b6b196facaecaea07b7e1e700f1adc03b423bb
UtopicRay <rayrm@gmail.com>
2024-10-25 02:45:51 -0400

    'Header Full Complete'

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,7 +29,20 @@ watch(group,()=>drawer.value=false);
       </v-btn>
     </template>
     <div v-else>
-      <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+        </template>
+
+        <v-list style="background-color: var(--background-color)">
+          <v-list-item
+              v-for="(item, i) in items" :key="i"
+              :href="item.value"
+              :title="item.title"
+          >
+           </v-list-item>
+        </v-list>
+      </v-menu>
     </div>
 </template>
 
